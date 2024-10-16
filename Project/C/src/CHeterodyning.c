c5c7283dc03ae8cdf7640c8291105dc0d3b027a8
Mahlomola mohlomi <141222052+Mahlomola2002@users.noreply.github.com>
2024-10-16 20:25:26 +0200

    Add files via upload
    added method that calculates the time

--- Project/C/src/CHeterodyning.c
+++ Project/C/src/CHeterodyning.c
@@ -11,14 +11,25 @@ int main(int argc, char**argv){
     
 
     printf("Total amount of samples: %ld\n", sizeof(data) / sizeof(data[0]));
-    
+    double avg_time=0;
 
-    tic(); // start the timer
+    for(int i=0;i<10000;i++){
+        avg_time=avg_time+calculate_time();
+    }
+    double t=avg_time/10000;
+
+   
+    printf("Time: %lf ms\n",t/1e-3);
+    printf("End Unthreaded Test\n");
+    printf("sizeof float: %lu\n", sizeof(float) * 8);
+    return 0;
+}
+
+double calculate_time(){
+     tic(); // start the timer
     for (int i = 0;i<SAMPLE_COUNT;i++ ){
         result[i] = data[i] * carrier[i];
     }
-    double t = toc();
-    printf("Time: %lf ms\n",t/1e-3);
-    printf("End Unthreaded Test\n");
-    return 0;
+    return toc();
+
 }
